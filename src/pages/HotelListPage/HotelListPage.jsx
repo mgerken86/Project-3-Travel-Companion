7bdb8e77c50a75c8f345be27425b300bf4c11f1c
Mark Gerken <mgerken86@gmail.com>
2022-07-05 14:39:45 -0600

    Markers state created on Hotels List Page to pass down to map to render markers for each hotel

--- src/pages/HotelListPage/HotelListPage.jsx
+++ src/pages/HotelListPage/HotelListPage.jsx
@@ -1,18 +1,34 @@
 import HotelListCard from "../../components/HotelListCard/HotelListCard";
 import Map from "../../components/Map/Map";
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function HotelsListPage() {
   const { state } = useLocation();
   // console.log(state);
   const { searchResult, checkIn, checkOut, coordinates } = state;
-  console.log(coordinates);
+  const [markers, setMarkers] = useState([])
+
+  useEffect(() => {
+    const getMarkers = () => {
+      searchResult.map(hotel => {
+        setMarkers({lat: hotel.latitude, lng: hotel.longitude})
+      })
+    }
+    getMarkers()
+  }, [])
+  useEffect(() => {
+    console.log(markers)
+  }, [markers])
+  
+
   return (
     <>
       <h1>Hotels List Page</h1>
       <Map
         lat={coordinates.lat}
         lng={coordinates.lng}
+        markers={markers}
       />
       <div>
         {searchResult.map((hotel) => {
