5491f4a40c67dd2caaab0f83052df03d63c554c1
Mark Gerken <mgerken86@gmail.com>
2022-07-05 12:27:18 -0600

    Work started on creating Map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,16 +1,20 @@
-const ref = React.useRef(null);
-const [map, setMap] = React.useState();
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { useMemo } from 'react';
 
-React.useEffect(() => {
-  if (ref.current && !map) {
-    setMap(new window.google.maps.Map(ref.current, {}));
-  }
-}, [ref, map]);
-
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 export default function Map() {
-    return <div>Map</div>
-}
-
-
-
+    // const { isLoaded } = useLoadScript({
+    //     googleMapsApiKey: GOOGLE_API_KEY,
+    //   });
+    
+    //   if (!isLoaded) return <div>Loading...</div>
+    //   return <Map />
+    // UseMemo prevents map from 're-centering' itself if user scrolls around on map
+    const center = useMemo(() => ({ lat: 44, lng: -80 }), [])
+    return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={{ lat: 44, lng: -80 }} />
+    </GoogleMap>
+    )
+  }
