5d5ef901bf3505265dfb16da4f4d3e1b383dc7bc
Mark Gerken <mgerken86@gmail.com>
2022-07-06 11:16:44 -0600

    More work on Trip Order history. Committing to merge from main after pull request from Jing

--- src/components/TripOrder/TripOrder.jsx
+++ src/components/TripOrder/TripOrder.jsx
@@ -4,6 +4,13 @@ export default function TripOrder({ trip }) {
     console.log(trip)
 
     return (
-        <h1>{trip._id}</h1>
+        <>
+        <h2>{trip.hotelName}</h2>
+        <h3>{trip.roomName}</h3>
+        <img src={trip.hotelPhoto} alt="" />
+        <p></p>
+        <p>Check-in: {trip.checkIn.slice(0, 10)} </p>
+        <p>Check-out: {trip.checkOut.slice(0, 10)}</p>
+        </>
     )
 }
--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -17,8 +17,8 @@ export default function AccountPage({ user }) {
 
     return (
         <>
-            <h1>Greetings, {user.name}</h1>
-            <h3>Your Previous Trips:</h3>
+            <h3>Greetings, {user.name}</h3>
+            <h1>Your Previous Trips:</h1>
             <TripOrderList tripOrders={tripOrders} />
             <h3>Edit Your Information</h3>
         </>
