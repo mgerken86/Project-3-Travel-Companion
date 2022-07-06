b6978d252eccad29b23c6ff09161a10210c4d7c0
Mark Gerken <mgerken86@gmail.com>
2022-07-06 11:40:48 -0600

    Updated the navigate route from clicking a map marker to include the number of people in the search in the url

--- src/pages/HotelListPage/HotelListPage.jsx
+++ src/pages/HotelListPage/HotelListPage.jsx
@@ -10,11 +10,14 @@ export default function HotelsListPage() {
   // console.log(state);
   const { searchResult, checkIn, checkOut, coordinates, numberOfPerson } =
     state;
-
+    console.log(searchResult)
+  console.log(checkIn, checkOut)
   const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     (() => {
+      const people = numberOfPerson
+      console.log('people:' , people)
       // console.log(searchResult)
       //When page renders, create an array of all of the lat/lon for each hotel
       //Then set the state of markers to that new array. Also passing hotel_id to redirect when clicking marker
@@ -24,6 +27,7 @@ export default function HotelsListPage() {
           hotelId: hotel.hotel_id,
           lat: hotel.latitude,
           lng: hotel.longitude,
+          people: people
         };
       });
       setMarkers(filteredMarkers);
