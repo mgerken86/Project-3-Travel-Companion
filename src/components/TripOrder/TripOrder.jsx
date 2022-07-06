5cf54a6478d08a4109bedf7fc6b0427c51fa8d67
Mark Gerken <mgerken86@gmail.com>
2022-07-06 14:18:51 -0600

    User is redirected back to Home Page after cancelling a trip

--- src/components/TripOrder/TripOrder.jsx
+++ src/components/TripOrder/TripOrder.jsx
@@ -1,7 +1,8 @@
-import * as ordersAPI from "../../utilities/tripOrders-api"; 
+import * as ordersAPI from "../../utilities/tripOrders-api";
 import { useNavigate } from "react-router-dom";
 
 export default function TripOrder({ trip }) {
+    const navigate = useNavigate()
     // console.log(trip)
     const handleCancelBtn = async (orderId) => {
         // console.log(orderId)
@@ -9,13 +10,18 @@ export default function TripOrder({ trip }) {
     }
     return (
         <>
-        <h2>{trip.hotelName}</h2>
-        <h3>{trip.roomName}</h3>
-        <img src={trip.hotelPhoto} alt="" />
-        <p></p>
-        <p>Check-in: {trip.checkIn.slice(0, 10)} </p>
-        <p>Check-out: {trip.checkOut.slice(0, 10)}</p>
-        <button onClick={() => handleCancelBtn(trip._id)}>Cancel This Trip</button>
+            <h2>{trip.hotelName}</h2>
+            <h3>{trip.roomName}</h3>
+            <img src={trip.hotelPhoto} alt="" />
+            <p></p>
+            <p>Check-in: {trip.checkIn.slice(0, 10)} </p>
+            <p>Check-out: {trip.checkOut.slice(0, 10)}</p>
+            <button onClick={() => {
+                handleCancelBtn(trip._id)
+                navigate('/')
+            }}>
+                Cancel This Trip
+            </button>
         </>
     )
 }
