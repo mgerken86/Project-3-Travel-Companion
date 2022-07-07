5964806c3f90e8409a60a5bed7551e842f5ee8c2
Mark Gerken <mgerken86@gmail.com>
2022-07-07 05:44:21 -0600

    Edit button added to TripOrder component

--- src/components/TripOrder/TripOrder.jsx
+++ src/components/TripOrder/TripOrder.jsx
@@ -1,9 +1,24 @@
 import * as ordersAPI from "../../utilities/tripOrders-api";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+
 
 export default function TripOrder({ trip }) {
     const navigate = useNavigate()
-    // console.log(trip)
+
+    // let checkInToString = trip.checkIn;
+    // let checkOutToString = trip.checkOut;
+    // useEffect(() => {
+
+    //     // This makes the format 'yyyy-mm-dd' 
+    //     checkInToString = checkInToString.toISOString().slice(0, 10);
+    //     checkOutToString = checkOutToString.toISOString().slice(0, 10);
+    // }, [])
+
+
+    console.log(trip)
+
     const handleCancelBtn = async (orderId) => {
         // console.log(orderId)
         await ordersAPI.cancelTrip(orderId);
@@ -22,6 +37,14 @@ export default function TripOrder({ trip }) {
                 navigate(0)
             }}>
                 Cancel This Trip
+            </button>
+            <button onClick={() => {
+                // we have to re-format our checkIn and checkOut dates to re-direct to show page
+                navigate(
+                    // `/hotels/${trip.hotelId}?checkin=${checkIn}&checkout=${checkOut}&numberOfPerson=${trip.numberOfPeople}`
+                )
+            }}>
+                Edit Your Stay at {trip.hotelName}
             </button>
         </>
     )
